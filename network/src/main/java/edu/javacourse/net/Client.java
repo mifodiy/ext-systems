0d3eedfe20eb6b0142651d69eef4cb44561f3b9d
mifodiy911 <mifodiy911@gmail.com>
2020-05-06 19:27:36 +0300

    add Greetable class and server.properties

--- network/src/main/java/edu/javacourse/net/Client.java
+++ network/src/main/java/edu/javacourse/net/Client.java
@@ -7,8 +7,8 @@ import java.time.LocalDateTime;
 public class Client
 {
     public static void main(String[] args) {
-        for (int i = 0; i<10; i++) {
-        SimpleClient sc = new SimpleClient();
+        for (int i = 0; i<5; i++) {
+        SimpleClient sc = new SimpleClient(i);
            sc.start();
        }
     }
@@ -16,16 +16,27 @@ public class Client
 
 class SimpleClient extends Thread{
 
+    private final static String[] COMMAND = {
+            "HELLO", "MORNING", "DAY","EVENING"
+    };
+
+    private int cmdNumber;
+
+    public SimpleClient(int cmdNumber){
+        this.cmdNumber = cmdNumber;
+    }
+
     @Override
     public void run(){
         try {
-            System.out.println("Started: " + LocalDateTime.now());
+//            System.out.println("Started: " + LocalDateTime.now());
             Socket socket = new Socket("127.0.0.1", 25225);
 
             BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
 
-            String sb = "Anton";
+            String command = COMMAND[cmdNumber % COMMAND.length];
+            String sb = command + " " + "Anton";
 
             bw.write(sb);
             bw.newLine();
@@ -36,7 +47,7 @@ class SimpleClient extends Thread{
 
             br.close();
             bw.close();
-            System.out.println("Finished: " + LocalDateTime.now());
+//            System.out.println("Finished: " + LocalDateTime.now());
         }catch (IOException ex){
             ex.printStackTrace(System.out);
         }
--- network/src/main/java/edu/javacourse/net/Server.java
+++ network/src/main/java/edu/javacourse/net/Server.java
@@ -1,28 +1,57 @@
 package edu.javacourse.net;
 
 import java.io.*;
+import java.lang.reflect.Constructor;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
 
 public class Server
 {
     public static void main(String[] args) throws IOException {
         ServerSocket socket = new ServerSocket(25225);
 
+        Map<String,Greetable> handlers = loadHandlers();
+
         System.out.println("Server is started");
         while (true){
             Socket client = socket.accept();
-            new SimpleServer(client).start();
+            new SimpleServer(client, handlers).start();
         }
+    }
+
+    private static Map<String, Greetable> loadHandlers() {
+        Map<String, Greetable> result = new HashMap<>();
+        try(InputStream is = Server.class.getClassLoader()
+                .getResourceAsStream("server.properties")){
+
+            Properties properties = new Properties();
+            properties.load(is);
+
+            for(Object command : properties.keySet()) {
+                String className = properties.getProperty(command.toString());
+                Class<Greetable> cl = (Class<Greetable>) Class.forName(className);
+                Greetable handler = cl.getConstructor().newInstance();
+                result.put(command.toString(), handler);
+            }
+        }catch (Exception ex){
+            ex.printStackTrace();
+            throw new RuntimeException(ex);
+        }
+        return result;
     }
 }
 
 class SimpleServer extends Thread{
 
     private Socket client;
+    private Map<String, Greetable> handlers;
 
-    public SimpleServer(Socket client){
+    public SimpleServer(Socket client, Map<String, Greetable> handlers){
         this.client = client;
+        this.handlers = handlers;
     }
 
     @Override
@@ -35,13 +64,18 @@ class SimpleServer extends Thread{
             BufferedReader br = new BufferedReader(new InputStreamReader(client.getInputStream()));
             BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
 
-            StringBuilder sb = new StringBuilder("Hello, ");
-            String userName = br.readLine();
-            System.out.println("server got string: " + userName);
-            Thread.sleep(2000);
 
-            sb.append(userName);
-            bw.write(sb.toString());
+            String request = br.readLine();
+            String[] lines = request.split("\\s+");
+            String command = lines[0];
+            String userName = lines[1];
+
+            System.out.println("server got string1: " + command);
+            System.out.println("server got string2: " + userName);
+//            Thread.sleep(2000);
+
+            String response = buildResponse(command, userName);
+            bw.write(response);
             bw.newLine();
             bw.flush();
 
@@ -52,5 +86,13 @@ class SimpleServer extends Thread{
         }catch (Exception ex){
             ex.printStackTrace(System.out);
         }
+    }
+
+    private String buildResponse(String command, String userName){
+        Greetable handler = handlers.get(command);
+        if(handler != null){
+            return handler.buildResponse(userName);
+        }
+        return "Hello, " + userName;
     }
 }
