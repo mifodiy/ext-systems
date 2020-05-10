4a72258cebf048e0273fb18583f2d66c040fa70a
mifodiy911 <mifodiy911@gmail.com>
2020-05-10 19:26:11 +0300

    create PoolConnectionBuilder

--- city-register/src/main/java/edu/javacourse/city/dao/PersonCheckDao.java
+++ city-register/src/main/java/edu/javacourse/city/dao/PersonCheckDao.java
@@ -20,12 +20,14 @@ public class PersonCheckDao
                     "p.date_of_birth = ? and a.street_code = ? and " +
                     "upper(a.building COLLATE \"en_US.UTF-8\") = upper(? COLLATE \"en_US.UTF-8\") and  ";
 
-    public PersonCheckDao() {
-        try{
-            Class.forName("org.postgresql.Driver");
-        } catch (Exception ex){
-            ex.printStackTrace();
-        }
+    private ConnectionBuilder connectionBuilder;
+
+    public void setConnectionBuilder(ConnectionBuilder connectionBuilder) {
+        this.connectionBuilder = connectionBuilder;
+    }
+
+    private Connection getConnection() throws SQLException {
+        return connectionBuilder.getConnection();
     }
 
     public PersonResponse checkPerson(PersonRequest request) throws PersonCheckException {
@@ -72,10 +74,5 @@ public class PersonCheckDao
         }
 
         return response;
-    }
-
-    private Connection getConnection() throws SQLException {
-        return DriverManager.getConnection("jdbc:postgresql://localhost/city_register",
-                "postgres", "postgres");
     }
 }
