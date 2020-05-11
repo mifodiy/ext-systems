2b94b116806e05255121402c6819720911c696bc
mifodiy911 <mifodiy911@gmail.com>
2020-05-11 13:21:34 +0300

    added ReST to the project and created LocalDateAdapter

---
+++ city-register/src/main/java/edu/javacourse/city/web/CheckPersonService.java
@@ -0,0 +1,22 @@
+package edu.javacourse.city.web;
+
+import edu.javacourse.city.domain.PersonRequest;
+import edu.javacourse.city.domain.PersonResponse;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/check")
+public class CheckPersonService
+{
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public PersonResponse checkPerson(PersonRequest request){
+        System.out.println(request.toString());
+        return new PersonResponse();
+    }
+}
