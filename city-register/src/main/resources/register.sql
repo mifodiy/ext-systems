be9f5d605206ac35b9fda9823ab74091195c3ace
mifodiy911 <mifodiy911@gmail.com>
2020-05-11 19:58:20 +0300

    change of wife`s address into register.sql

--- city-register/src/main/resources/register.sql
+++ city-register/src/main/resources/register.sql
@@ -86,7 +86,7 @@ INSERT INTO cr_address_person (address_id, person_id, start_date, end_date)
 VALUES ( 1, 1, '2014-10-12', null);
 
 INSERT INTO cr_address_person (address_id, person_id, start_date, end_date)
-VALUES ( 2, 2, '2014-10-12', null);
+VALUES ( 1, 2, '2014-10-12', null);
 
 INSERT INTO cr_address_person (address_id, person_id, start_date, end_date)
 VALUES ( 1, 3, '2016-02-05', null);
