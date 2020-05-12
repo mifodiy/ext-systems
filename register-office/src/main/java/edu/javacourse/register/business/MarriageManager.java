32a67287ccbf11e67bed3448ee796321e07498a2
mifodiy911 <mifodiy911@gmail.com>
2020-05-12 20:29:35 +0300

    create class for domain model

---
+++ register-office/src/main/java/edu/javacourse/register/business/MarriageManager.java
@@ -0,0 +1,11 @@
+package edu.javacourse.register.business;
+
+import edu.javacourse.register.view.MarriageRequest;
+import edu.javacourse.register.view.MarriageResponse;
+
+public class MarriageManager
+{
+    public MarriageResponse findMarriageCertificate(MarriageRequest request){
+        throw new UnsupportedOperationException("unsupported");
+    }
+}
