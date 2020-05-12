d4a42b0729751b12bb90f9a527ced879df630d10
mifodiy911 <mifodiy911@gmail.com>
2020-05-12 15:08:17 +0300

    connection Hibernate to DB with JPA

--- register-office/src/main/java/edu/javacourse/register/manager/PersonManager.java
+++ register-office/src/main/java/edu/javacourse/register/manager/PersonManager.java
@@ -8,13 +8,46 @@ import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
-import java.io.Serializable;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 import java.util.List;
 
 public class PersonManager
 {
     public static void main(String[] args) {
 
+//        sessionExample();
+
+        jpaExample();
+
+    }
+
+    private static void jpaExample() {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("persistence");
+
+        EntityManager em = emf.createEntityManager();
+
+        em.getTransaction().begin();
+
+        Person p = new Person();
+        p.setFirstName("Алексей");
+        p.setLastName("Федоров");
+        em.persist(p);
+        System.out.println(p.getPersonId());
+
+        em.getTransaction().commit();
+        em.close();
+
+        em = emf.createEntityManager();
+        List list = em.createQuery("FROM Person").getResultList();
+        list.forEach(p1 -> System.out.println(p1));
+
+        em.close();
+
+    }
+
+    private static void sessionExample() {
         SessionFactory sf = buildSessionFactory();
 
         Session session = sf.openSession();
@@ -41,7 +74,6 @@ public class PersonManager
 
         List<Person> list = session.createQuery("FROM Person", Person.class).list();
         list.forEach(p1 -> System.out.println(p1));
-
     }
 
     private static SessionFactory buildSessionFactory() {
