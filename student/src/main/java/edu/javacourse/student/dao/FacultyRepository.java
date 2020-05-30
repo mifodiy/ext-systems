0a74041b8c008fefe00b369e8d7041111c6c6784
mifodiy911 <mifodiy911@gmail.com>
2020-05-30 19:08:15 +0300

    solved the problem LazyInitializationException

---
+++ student/src/main/java/edu/javacourse/student/dao/FacultyRepository.java
@@ -0,0 +1,9 @@
+package edu.javacourse.student.dao;
+
+import edu.javacourse.student.domain.Faculty;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FacultyRepository extends JpaRepository<Faculty, Long>
+{
+
+}
